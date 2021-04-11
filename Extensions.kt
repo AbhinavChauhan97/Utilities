a65274e21bfba5adbcead4cc186a8a5627c138fb
Abhinav Chauhan <47742471+AbhinavChauhan97@users.noreply.github.com>
2021-04-11 12:40:42 +0530

    Update Extensions.kt

--- Extensions.kt
+++ Extensions.kt
@@ -7,6 +7,9 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
+/**
+ method of return the last location in sychronous fasion so that you don't need to use any callback in your code 
+ @return the last location of the device **/
 @SuppressLint("MissingPermission")
 suspend fun FusedLocationProviderClient.awaitLastLocation() =
     suspendCoroutine<Location> { continuation ->
@@ -16,4 +19,18 @@ suspend fun FusedLocationProviderClient.awaitLastLocation() =
             continuation.resumeWithException(exception)
         }
     }
+
+/**
+ method returns the drawble associated with the provided id
+ this method just makes your code smaller , now in your activity you don't have to say
+ ContextCompat.getDrawble(this,R.id.your_drawble) just say getAsDrawble(R.id.your_drawble)
+**/
+Context.getAsDrawble(id:Int) = ContextCompat.getDrawble(this,id)
+
+/**
+similar to above to use in frament without using requireActivity().getAsDrawble(Int) 
+**/
+
+Fragment.getAsDrawble(id:Int) = ContextCompat.getDrawble(this.requireActivity(),id)
+
 
