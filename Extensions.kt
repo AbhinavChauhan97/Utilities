7b45657dc8f81bcd3815a999c672ef1277d06e23
Abhinav Chauhan <47742471+AbhinavChauhan97@users.noreply.github.com>
2021-04-17 18:53:45 +0530

    Update Extensions.kt

--- Extensions.kt
+++ Extensions.kt
@@ -58,4 +58,19 @@ fun Context.networkAvailableFlow(): Flow<Boolean> = callbackFlow {
     }
 }
 
+fun EditText.afterTextChangedFlow(): Flow<Editable?> 
+    = callbackFlow {
+        val watcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                offer(s)
+            }
+            override fun beforeTextChanged(s: CharSequence?, 
+                               start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, 
+                               start: Int, before: Int, count: Int) {}
+        }
+        addTextChangedListener(watcher)
+        awaitClose { removeTextChangedListener(watcher) }
+    }
+
 
